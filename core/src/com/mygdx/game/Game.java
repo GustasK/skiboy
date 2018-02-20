b745b81df98283c3aa646bb604db43c9fbd5f104
Gustas Kurtkus <gusteakask@gmail.com>
2018-02-20 22:39:16 +0000

    GameStateManager created to manage game states

--- core/src/com/mygdx/game/Game.java
+++ core/src/com/mygdx/game/Game.java
@@ -5,24 +5,34 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.game.States.GameStateManager;
+import com.mygdx.game.States.MenuState;
 
 public class Game extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+
+	public static final int WIDTH = 480;
+	public static final int HEIGHT = 800;
+
+	public static final String TITLE = "SkiBoy";
+
+	private GameStateManager gameStateManager;
+	private SpriteBatch batch;
+	private Texture img;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		gameStateManager = new GameStateManager();
+
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		gameStateManager.push(new MenuState(gameStateManager));
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);	/*	wipes the screen	*/
+		gameStateManager.update(Gdx.graphics.getDeltaTime());
+		gameStateManager.render(batch);
 	}
 	
 	@Override
---
+++ core/src/com/mygdx/game/States/MenuState.java
@@ -0,0 +1,26 @@
+package com.mygdx.game.States;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/**
+ * Created by User on 2/20/2018.
+ */
+
+public class MenuState extends State {
+
+    public MenuState(GameStateManager gameStateManager) {
+        super(gameStateManager);
+    }
+
+    public void handleInput() {
+
+    }
+
+    public void update(float dt) {
+
+    }
+
+    public void render(SpriteBatch sb) {
+
+    }
+}
